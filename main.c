d991d032bcbe3e64f681d744c47f0d94581f3aa8
Dremziorrr <the_dr.emz@proton.me>
2024-12-03 23:12:38 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+
+    if (argc < 2) {
+        printf("Usage: %s <Nazwa pliku>", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    FILE *f = fopen((char*)argv[1], "r");
+    if (f == NULL) {
+        printf("Blad otwierania pliku");
+        return EXIT_FAILURE;
+    }
+
+    int i = 1; 
+    int j = 0;
+    char buff[CHAR_MAX];
+    char wynik[CHAR_MAX];
+    while (fgets(buff, 127, f) != NULL) {
+        //printf("%lld,", strlen(buff));
+        if (i == 40) {
+            //printf("%s", buff);
+            wynik[j] = buff[9];
+            j++;
+            i = 0;
+        }
+        i++;
+    }
+    strcpy(&wynik[j], "\0");
+
+    printf("%s", wynik);
+
+    fclose(f);
+    return EXIT_SUCCESS;
+}
